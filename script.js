c16cc53f7db22c86f170bdebb4932e0ace0111e7
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-09-26 20:49:39 -0300

    Adding pre-load animation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Site ainda em construção!")
+let element_p = document.getElementById("preloader");
+let element_l = document.getElementById("loader");
+console.log("ok, passou por aqui!");
+
+setTimeout(function(){
+    element_p.classList.remove("preloader");
+    element_l.classList.remove("loader");
+},1280);
