6df27fc74d332e8ccdb0fdfce61a067f22a2a923
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-01-15 16:29:48 -0300

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,40 @@
+
+let nome = document.getElementById('name');
+
+let progress = 0;
+let myName = '[Gabriel do Amaral]';
+
+const type = ()=>{       
+    nome.textContent += myName.charAt(progress);
+    progress++;
+
+    if(progress === myName.length){
+        progress = 0;
+        clearInterval(intervalo);
+        
+    }
+}
+
+let intervalo = setInterval(type,100);
+
+//efeito fade in
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeElements = document.querySelectorAll(".fade-in-element");
+  
+    function fadeInOnScroll() {
+      fadeElements.forEach(function(element) {
+        var elementPosition = element.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight;
+  
+        if (elementPosition < screenPosition && element.style.opacity !== "1") {
+          element.style.opacity = "1";
+        }
+      });
+    }
+  
+    // chamando o fade in
+    fadeInOnScroll();
+  
+    // Fade in on scroll
+    window.addEventListener("scroll", fadeInOnScroll);
+});
---
+++ assets/scripts/script.js
@@ -0,0 +1,67 @@
+//passar o texto para a linha de cima
+function handleInput(event) {
+    if (event.key === 'Enter') {
+      executeCommand();
+    }
+  }
+
+  function executeCommand() {
+    var userInput = document.getElementById('user-input').value;
+    if(userInput.toLowerCase() == '$ start'){
+        document.getElementById('output').innerHTML = '$ entrando...';
+        document.getElementById('user-input').value = '$ aguarde...';
+        let intervalId = setInterval(loading, 300);
+    }else{
+        document.getElementById('output').innerHTML = '$ Comando não identificado!';
+        document.getElementById('user-input').value = '$ ';
+    }
+  }
+
+//efeito fade in
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeElements = document.querySelectorAll(".fade-in-element");
+  
+    function fadeInOnScroll() {
+      fadeElements.forEach(function(element) {
+        var elementPosition = element.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight;
+  
+        if (elementPosition < screenPosition && element.style.opacity !== "1") {
+          element.style.opacity = "1";
+        }
+      });
+    }
+  
+    // chamando o fade in
+    fadeInOnScroll();
+  
+    // Fade in on scroll
+    window.addEventListener("scroll", fadeInOnScroll);
+});
+
+//funcao focus input
+function digitarInput(){
+    let cmd = document.getElementById('user-input');
+    window.onload = ()=> cmd.focus();
+    cmd.value = '$ ';
+
+}
+digitarInput();
+
+
+//funcao loading
+let progress = 0;
+let progressBar = document.getElementById('progress');
+
+function loading(){
+    if(progress<100){
+        progress += 10;
+        progressBar.style.width = progress + '%';
+        progressBar.innerHTML = '[' + '='.repeat(progress / 10) + '>' + ' '.repeat(10 - progress / 10) + ']' + progress + '%';
+    }else{
+        window.location.href = "portfolio.html";
+        clearInterval(intervalId);
+    }
+
+}
+
